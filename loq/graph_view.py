45796c23c86bfc46e58f3a8ede0ec870f75e788c
Alvin <alvinwtng@gmail.com>
2014-01-09 15:00:29 +0800

    # Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.# On branch master# Changes to be committed:#   (use "git reset HEAD <file>..." to unstage)#	modified:   loq/graph_view.py## Changes not staged for commit:#   (use "git add/rm <file>..." to update what will be committed)#   (use "git checkout -- <file>..." to discard changes in working directory)##	deleted:    loq/#graph_view.py##	modified:   loq/graph_view.pyc## Untracked files:#   (use "git add <file>..." to include in what will be committed)##	README~#	dist_1dme-mir-307a.png#	dist_dme-mir-307a.png#	dist_dme-mir-965.png#	dist_percent_dme-mir-283.png#	dist_percent_dme-mir-305.png#	dist_percent_dme-mir-307a.png#	dist_percent_dme-mir-311.png#	dist_percent_dme-mir-316.png#	dist_percent_dme-mir-318.png#	dist_percent_dme-mir-79.png#	dist_percent_dme-mir-87.png#	dist_percentdme-mir-307a.png#	dist_splitdme-mir-307a.png#	foo.png#	loq.png#	loq_percent_dist/#	nohup.out#	static/

--- loq/graph_view.py
+++ loq/graph_view.py
@@ -18,6 +18,53 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 
 sns.set(style='whitegrid',palette=["#000000", "#E69F00", "#56B4E9", "#009E73", "#8C1515", "#D55E00", "#CC79A7"],context='talk')
 
+
+def plot_graph(name,libraries,chart_type,normal):
+    interval_object = Interval.objects.select_related().get(mirName__icontains=name)
+    read_object= interval_object.read_alignment_set.filter(lib__in=libraries).order_by('lib')
+    len_count =read_object.values('read_length','lib','read_counts','intervalName__sum_read_counts','normReads').annotate(Count('read_length')).order_by('read_length')
+    lib_total_count =read_object.values('lib').annotate(Sum('read_counts'))
+
+    length=[]
+    for i in len_count:
+        length.append(int(i['read_length']))
+        lengths_reads = sorted(set(length))
+                
+    df = pd.DataFrame(index=lengths_reads, columns=libraries)
+    df = df.fillna(0).convert_objects()
+    d= {}
+                
+    for j in lib_total_count:
+        d[j['lib']] = j['read_counts__sum']
+                        
+    for i in len_count:
+        lb=i['lib']
+        lh=i['read_length']
+        if normal == 'dist_rpm':
+            data = int(i['read_length__count']*i['normReads'])
+            title = 'Reads per million'
+        elif normal == 'dist_read_counts':
+            data = int(i['read_length__count']*i['read_counts'])
+            title = 'Reads Counts'
+        else: 
+            data = int((100*i['read_length__count']*i['read_counts'])/d[i['lib']])
+            title ='Read counts per miRNA mapped'
+        df[lb][lh] += data
+          
+    fig= plt.figure()
+    ax = fig.add_subplot(111)
+    df.plot(ax=ax, kind=chart_type,figsize=(10,8),subplots=False,)
+    plt.title('Read Length Distribution of '+ str(interval_object.mirName))
+    plt.xlabel('Length of Read')
+    plt.grid(True)
+    plt.ylabel(title)
+    canvas = FigureCanvas(fig)
+    response = HttpResponse(mimetype='image/jpg')
+    canvas.print_png(response)
+    return response
+
+
+
 def Graph_Form(request):
     errors = []
     if request.user.is_authenticated():
@@ -31,48 +78,9 @@ def Graph_Form(request):
                 chart_type = form.cleaned_data['Chart']
                 normal = form.cleaned_data['Normal']
                 try:
-                    interval_object = Interval.objects.select_related().get(mirName__icontains=name)
-                    read_object= interval_object.read_alignment_set.filter(lib__in=libraries).order_by('lib')
-                    len_count =read_object.values('read_length','lib','read_counts','intervalName__sum_read_counts','normReads').annotate(Count('read_length')).order_by('read_length')
-                    lib_total_count =read_object.values('lib').annotate(Sum('read_counts'))
-                    length=[]
-                    for i in len_count:
-                        length.append(int(i['read_length']))
-                        lengths_reads = sorted(set(length))
-                
-                    df = pd.DataFrame(index=lengths_reads, columns=libraries)
-                    df = df.fillna(0).convert_objects()
-                    d= {}
-                
-                    for j in lib_total_count:
-                        d[j['lib']] = j['read_counts__sum']
-                        
-                    for i in len_count:
-                        lb=i['lib']
-                        lh=i['read_length']
-                        if normal == 'dist_rpm':
-                            data = int(i['read_length__count']*i['normReads'])
-                            title = 'Reads per million'
-                        elif normal == 'dist_read_counts':
-                            data = int(i['read_length__count']*i['read_counts'])
-                            title = 'Reads Counts'
-                        else: 
-                            data = int((100*i['read_length__count']*i['read_counts'])/d[i['lib']])
-                            title ='Read counts per miRNA mapped'
-                        df[lb][lh] += data
-          
-                    fig= plt.figure()
-                    ax = fig.add_subplot(111)
-                    df.plot(ax=ax, kind=chart_type,figsize=(10,8),subplots=False,)
-                    plt.title('Read Length Distribution of '+ str(interval_object.mirName))
-                    plt.xlabel('Length of Read')
-                    plt.grid(True)
-                    plt.ylabel(title)
-                    canvas = FigureCanvas(fig)
-                    response = HttpResponse(mimetype='image/jpg')
-                    canvas.print_png(response)
-                    return response
-                # return HttpResponseRedirect(reverse('IntervalDetailView', kwargs={'pk': interval_object.pk}))
+                    response =  plot_graph(name,libraries,chart_type,normal)
+                    return response 
+                    # return HttpResponseRedirect(reverse('IntervalDetailView', kwargs={'pk': interval_object.pk}))
                 except (MultipleObjectsReturned), e :
                     return HttpResponse("Please provide a specific query. Your search returned more than one result.")
             
